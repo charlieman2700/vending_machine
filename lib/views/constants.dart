1d6583010b82946f1009d3b5b08207a0fe876ef5
Carlos <carlos.ace27@gmail.com>
2022-07-27 15:58:33 -0600

    Se crean constantes de colores

---
+++ lib/views/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const color1 = Color.fromARGB(207, 226, 124, 96);
+const color2 = Color.fromARGB(255, 8, 92, 203);
+const color3 = Color.fromARGB(255, 232, 167, 124);
+const color4 = Color.fromARGB(255, 195, 141, 158);
+const color5 = Color.fromARGB(255, 65, 179, 164);
+const color6 = Color.fromARGB(255, 116, 199, 188);
