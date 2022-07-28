b00f660a8f66cf13f5b063a9cd889509716a0268
Carlos <carlos.ace27@gmail.com>
2022-07-27 22:07:51 -0600

    Se separan widgets y se implementa el carrito de compras

---
+++ lib/widgets/drinks.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:vending_machine/widgets/soda_info.dart';
+
+class Drinks extends StatelessWidget {
+  const Drinks({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsetsDirectional.all(10),
+        margin: const EdgeInsets.all(23),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                SodaInfo(
+                    imageUrl: 'assets/images/bottles/can/spriteCan.png',
+                    textName: 'Sprite',
+                    productID: '4'),
+                SodaInfo(
+                    imageUrl: 'assets/images/bottles/can/fantaCan.png',
+                    textName: 'Fanta',
+                    productID: '3'),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  SodaInfo(
+                    imageUrl: 'assets/images/bottles/can/cokeCan.png',
+                    textName: 'Coca Cola',
+                    productID: '1',
+                  ),
+                  SodaInfo(
+                    imageUrl: 'assets/images/bottles/can/pepsiCan.png',
+                    textName: 'Pepsi',
+                    productID: '2',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/soda_info.dart
+++ lib/widgets/soda_info.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vending_machine/controllers/vending_machine_controller.dart';
 
-import '../views/constants.dart';
+import '../views/constants.dart' as theme;
 
 class SodaInfo extends GetView<VendingMachineController> {
   final String imageUrl;
@@ -22,40 +22,93 @@ class SodaInfo extends GetView<VendingMachineController> {
 
   @override
   Widget build(BuildContext context) {
-    Color buttonsColor = color5;
-    return Column(
-      children: [
-        Image.asset(imageUrl, fit: BoxFit.cover, width: width, height: height),
-        Container(
-          margin: const EdgeInsets.only(top: 10),
-          child: Column(
-            children: [
-              Text(
-                textName,
-                style: const TextStyle(color: Colors.black, fontSize: 25),
-              ),
-              Obx(
-                () => Text(
-                  'In Stock: ${controller.getProductAmountById(productID)}',
-                  style: const TextStyle(color: Colors.black, fontSize: 25),
+    Color buttonsColor = theme.color5;
+    double buttonSize = 32;
+    return SizedBox(
+      width: 200,
+      child: Column(
+        children: [
+          Image.asset(imageUrl,
+              fit: BoxFit.cover, width: width, height: height),
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      textName,
+                      style: const TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text(
+                      '(₡${controller.priceOfProductByID(productID).toString()})',
+                      style: const TextStyle(color: Colors.black, fontSize: 21),
+                    ),
+                  ],
                 ),
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.add_circle,
-                    color: buttonsColor,
+                Divider(
+                  height: theme.drinksTextDivider + 8,
+                  indent: 24,
+                  endIndent: 24,
+                  thickness: 1.3,
+                ),
+                Obx(
+                  () => Text(
+                    'Available: ${controller.getProductAmountById(productID)}',
+                    style: const TextStyle(color: Colors.black, fontSize: 25),
                   ),
-                  Icon(
-                    Icons.remove_circle,
-                    color: buttonsColor,
-                  )
-                ],
-              )
-            ],
-          ),
-        )
-      ],
+                ),
+                Divider(
+                  height: theme.drinksTextDivider,
+                  color: Colors.black87.withOpacity(0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () =>
+                          {controller.removeProductFromShoppingCart(productID)},
+                      child: Icon(
+                        Icons.remove_circle,
+                        color: buttonsColor,
+                        size: buttonSize,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Obx(
+                      () => Text(
+                        controller
+                            .quantityOfProductInShoppingCart(productID)
+                            .toString(),
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 25),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    InkWell(
+                      onTap: () =>
+                          {controller.addProductToShoppingCart(productID)},
+                      child: Icon(
+                        size: buttonSize,
+                        Icons.add_circle,
+                        color: buttonsColor,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
