def71a38954bc573a90d604ffe039e2419790bbe
Carlos <carlos.ace27@gmail.com>
2022-07-27 22:06:34 -0600

    Se crean funciones en el controlador para funcionar de intermediario

--- lib/controllers/vending_machine_controller.dart
+++ lib/controllers/vending_machine_controller.dart
@@ -4,13 +4,57 @@ import 'package:vending_machine/models/vending_machine.dart';
 
 class VendingMachineController extends GetxController {
   final vendingMachine = VendingMachine.defaultProductStock().obs;
+  final productsInShoppingCart = <Product, int>{}.obs;
+  final totalShoppingCart = 0.obs;
 
-  int getProductAmountById(ProductID productId) {
+  int getProductAmountById(ProductID productID) {
     int stockQuantity = 0;
-    var result = vendingMachine.value.getProductStock()[productId];
+    var result = vendingMachine.value.getProductStock()[productID];
     if (result != null) {
       stockQuantity = result;
     }
     return stockQuantity;
+  }
+
+  void addProductToShoppingCart(ProductID productID) {
+    var result = vendingMachine.value.getProductStock()[productID];
+    if (result != null) {
+      vendingMachine.value.addProductToShoppingCart(productID);
+      _updateShoppingCart();
+    }
+  }
+
+  void removeProductFromShoppingCart(ProductID productID) {
+    var result = vendingMachine.value.getProductStock()[productID];
+    if (result != null) {
+      vendingMachine.value.removeFromShoppingCart(productID);
+      _updateShoppingCart();
+    }
+  }
+
+  int quantityOfProductInShoppingCart(ProductID productID) {
+    return vendingMachine.value
+        .getShoppingCart()
+        .productQuantityInCart(productID);
+  }
+
+  int priceOfProductByID(ProductID productID) {
+    return vendingMachine.value.getProductCostByID(productID);
+  }
+
+  Map<Product, int> getProductsOnShoppingCart() {
+    return vendingMachine.value.getShoppingCart().getProductsInCart();
+  }
+
+  bool shoppingCartIsEmpty() {
+    return vendingMachine.value.getShoppingCart().cartIsEmpy();
+  }
+
+  void _updateShoppingCart() {
+    productsInShoppingCart.value =
+        vendingMachine.value.getShoppingCart().getProductsInCart();
+    totalShoppingCart.value =
+        vendingMachine.value.getShoppingCart().getTotalPrice();
+    vendingMachine.refresh();
   }
 }
