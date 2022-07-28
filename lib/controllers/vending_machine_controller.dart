935173cc244df52a0cbe67b19472137a79d46d82
Carlos <carlos.ace27@gmail.com>
2022-07-28 02:18:28 -0600

    Se crean funciones necesarias para la visualización del ingreso de dinero

--- lib/controllers/vending_machine_controller.dart
+++ lib/controllers/vending_machine_controller.dart
@@ -2,10 +2,49 @@ import 'package:get/get.dart';
 import 'package:vending_machine/models/product.dart';
 import 'package:vending_machine/models/vending_machine.dart';
 
+// Este controlador implementa el getxController para administrar estados
+// Su funcion principal es ser un puente entre el modelo y la vista
 class VendingMachineController extends GetxController {
   final vendingMachine = VendingMachine.defaultProductStock().obs;
   final productsInShoppingCart = <Product, int>{}.obs;
   final totalShoppingCart = 0.obs;
+  final payment = <int, int>{500: 0, 100: 0, 50: 0, 25: 0}.obs;
+
+  bool isEnoughToPay() {
+    int moneyReceived = getTotalAmountPayment();
+    bool isEnoughToPay = vendingMachine.value
+        .getMoneyManager()
+        .isEnoughToPay(totalShoppingCart.value, moneyReceived);
+    return isEnoughToPay;
+  }
+
+  bool canGiveChange() {
+    return true;
+  }
+
+  void addCoinsToMachine() {
+    payment.forEach((coinType, quantity) {
+      vendingMachine.value.getMoneyManager().addCoins(coinType, quantity);
+    });
+  }
+
+  void addCoinToPayment(int coinType) {
+    payment[coinType] = payment[coinType]! + 1;
+  }
+
+  void removeCoinFromPayment(int coinType) {
+    if (payment[coinType]! > 0) {
+      payment[coinType] = payment[coinType]! - 1;
+    }
+  }
+
+  int getTotalAmountPayment() {
+    int totalAmount = 0;
+    payment.forEach((coinType, quantity) {
+      totalAmount += coinType * quantity;
+    });
+    return totalAmount;
+  }
 
   int getProductAmountById(ProductID productID) {
     int stockQuantity = 0;
@@ -14,6 +53,10 @@ class VendingMachineController extends GetxController {
       stockQuantity = result;
     }
     return stockQuantity;
+  }
+
+  List<int> getCoinTypes() {
+    return vendingMachine.value.getCoinTypes();
   }
 
   void addProductToShoppingCart(ProductID productID) {
