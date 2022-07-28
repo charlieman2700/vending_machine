7a2b332972e4febee2f4b2ed971caf40f5eaab3d
Carlos <carlos.ace27@gmail.com>
2022-07-28 02:16:34 -0600

    Se corrige Error de topografia de widgets

--- lib/widgets/drinks.dart
+++ lib/widgets/drinks.dart
@@ -8,45 +8,43 @@ class Drinks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        padding: const EdgeInsetsDirectional.all(10),
-        margin: const EdgeInsets.all(23),
-        child: Column(
-          children: [
-            Row(
+    return Container(
+      padding: const EdgeInsetsDirectional.all(10),
+      margin: const EdgeInsets.all(23),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              SodaInfo(
+                  imageUrl: 'assets/images/bottles/can/spriteCan.png',
+                  textName: 'Sprite',
+                  productID: '4'),
+              SodaInfo(
+                  imageUrl: 'assets/images/bottles/can/fantaCan.png',
+                  textName: 'Fanta',
+                  productID: '3'),
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 30),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: const [
                 SodaInfo(
-                    imageUrl: 'assets/images/bottles/can/spriteCan.png',
-                    textName: 'Sprite',
-                    productID: '4'),
+                  imageUrl: 'assets/images/bottles/can/cokeCan.png',
+                  textName: 'Coca Cola',
+                  productID: '1',
+                ),
                 SodaInfo(
-                    imageUrl: 'assets/images/bottles/can/fantaCan.png',
-                    textName: 'Fanta',
-                    productID: '3'),
+                  imageUrl: 'assets/images/bottles/can/pepsiCan.png',
+                  textName: 'Pepsi',
+                  productID: '2',
+                ),
               ],
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  SodaInfo(
-                    imageUrl: 'assets/images/bottles/can/cokeCan.png',
-                    textName: 'Coca Cola',
-                    productID: '1',
-                  ),
-                  SodaInfo(
-                    imageUrl: 'assets/images/bottles/can/pepsiCan.png',
-                    textName: 'Pepsi',
-                    productID: '2',
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
